2aa9d7093b4e5681b884d50f87e82333b6e0b3c6
Mitchell <mitch.heidbrink@gmail.com>
2026-02-14 23:06:32 -0500

    add http server and request handler, using service function

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,40 @@
-use tokio::net::TcpListener;
-use tracing::{info, error};
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use tracing::{info};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initilize structured logging
     tracing_subscriber::fmt::init();
 
-    let addr = "127.0.0.1:8080";
+    let addr = ([127, 0, 0, 1], 8080).into();
 
-    info!("Starting TCP port listener on {}", addr);
+    info!("Starting HTTP server on {}", addr);
 
-    let listener = TcpListener::bind(addr).await?;
+//    let listener = TcpListener::bind(addr).await?;
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle_request))
+    });
+    
+    let server = Server::bind(&addr).serve(make_svc);
 
-    loop {
-        match listener.accept().await {
-            Ok((socket, peer_addr)) => {
-                info!("New connection from {}", peer_addr);
+    server.await?;
 
-                // Spawn a task per connection
-                tokio::spawn(async move {
-                    if let Err(e) = handle_connection(socket).await {
-                        error!("Connection error: {:?}", e);
-                    }
-                });
-            }
-            Err(e) => {
-                error!("Failed to accept connection {:?}", e);
-            }
-        }
-    }
+    Ok(())
 }
 
-async fn handle_connection(
-    _socket: tokio::net::TcpStream,
-) -> Result<(), Box<dyn std::error::Error>> {
+async fn handle_request(
+    req: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
     // For now, do nothing
-    Ok(())
+    info!(
+        method = ?req.method(),
+        path = ?req.uri().path(),
+        "Received Request"
+    );
+    
+    let response = Response::new(Body::from("Hello from Rust L6 Proxy"));
+
+    Ok(response)
 }
