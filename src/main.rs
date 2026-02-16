596453f2351154a2e2deb68e8c2986e9da76ee3d
Mitchell <mitch.heidbrink@gmail.com>
2026-02-15 23:18:36 -0500

    add connection pooling, client and request format to upstream server, create proxy_request

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use hyper::{Body, Request, Response, Server};
+use hyper::client::HttpConnector;
+use hyper::{Body, Client, Request, Response, Server, Uri};
 use hyper::service::{make_service_fn, service_fn};
 use std::convert::Infallible;
-use tracing::{info};
+use tracing::{info, error};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,10 +14,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Starting HTTP server on {}", addr);
 
 //    let listener = TcpListener::bind(addr).await?;
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle_request))
-    });
     
+    // Hyper client with conneciton pooling built in
+    let client: Client<HttpConnector, Body> = Client::new();
+
+    let make_svc = make_service_fn(move |_conn| {
+        let client = client.clone();
+
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                proxy_request(req, client.clone())
+            }))
+        }
+    });
+
     let server = Server::bind(&addr).serve(make_svc);
 
     server.await?;
@@ -24,17 +35,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn handle_request(
-    req: Request<Body>,
+async fn proxy_request(
+    mut req: Request<Body>,
+    client: Client<HttpConnector, Body>,
 ) -> Result<Response<Body>, Infallible> {
-    // For now, do nothing
+    let upstream = "http://127.0.0.1:9001";
+
     info!(
         method = ?req.method(),
         path = ?req.uri().path(),
-        "Received Request"
+        "Proxying request"
     );
-    
-    let response = Response::new(Body::from("Hello from Rust L6 Proxy"));
 
-    Ok(response)
+    // Build new URI pointing to upstream
+    let new_uri = format!(
+        "{}{}",
+        upstream,
+        req.uri().path_and_query()
+            .map(|x| x.as_str())
+            .unwrap_or("/")
+    );
+
+    *req.uri_mut() = new_uri.parse::<Uri>().unwrap();
+
+    match client.request(req).await {
+        Ok(res) => {
+            info!("Upstream responded with {}", res.status());
+            Ok(res)
+        }
+        Err(err) => {
+            error!("Upstream error: {:?}", err);
+
+            let response = Response::builder()
+                .status(502)
+                .body(Body::from("Bad Gateway"))
+                .unwrap();
+
+            Ok(response)
+        }
+    }
 }
